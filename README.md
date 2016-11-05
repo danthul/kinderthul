d8f75cc02f3016eb87355e61da196034f83d5d43
Dan Thul <dan.j.thul@temeda.com>
2016-11-05 10:12:03 -0500

    Cleaning history from react slingshot

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kinderthul!
+
+Kinderthul
+
+A kindergarten classroom site built using the React Slingshot starter kit from Cory House
+
---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Kinderthul</h1>
+
+      <h2>Here we go</h2>
+      <ol>
+        <li>Just a list</li>
+        <li>Just a list 2</li>
+      </ol>
+    </div>
+  );
+};
+
+export default HomePage;
